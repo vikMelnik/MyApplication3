f24d855a16f2b13758432f53d2c5cd2902fd4a73
ViktorMel <vitekm@ukr.net>
2022-03-24 09:52:09 +0200

    Простой калькулятор(сырая версия)

--- app/src/main/java/come/geekbrains/myapplication3/MainActivity.java
+++ app/src/main/java/come/geekbrains/myapplication3/MainActivity.java
@@ -3,12 +3,161 @@ package come.geekbrains.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  private EditText mEditText;
+  SimpleCalculator mSmplCalc;
+  private boolean mPlus, mDivide, mMulti, mMinus, mSquare;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    initView();
+  }
+
+  private void initView() {
+    mSmplCalc = new SimpleCalculator();
+    mEditText = (EditText) findViewById(R.id.editText);
+    ((Button) findViewById(R.id.button_0)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_1)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_2)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_3)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_4)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_5)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_6)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_7)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_8)).setOnClickListener(this);
+    ((Button) findViewById(R.id.button_9)).setOnClickListener(this);
+    ((Button) findViewById(R.id.butt_point)).setOnClickListener(this);
+    initButtonPlusClickListener();
+    initButtonDivideClickListener();
+    initButtonMultiClickListener();
+    initButtonMinusClickListener();
+    initButtonSquareClickListener();
+    initButtonEqualClickListener();
+    initButtonDeleteClickListener();
+  }
+
+  private void initButtonDeleteClickListener() {
+    ((Button) findViewById(R.id.butt_delete)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mEditText.setText(null);
+      }
+    });
+  }
+
+  private void initButtonSquareClickListener() {
+    ((Button) findViewById(R.id.butt_square)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (mEditText == null) {
+          mEditText.setText("");
+        } else {
+          mSmplCalc.setNumOne(Double.parseDouble(mEditText.getText() + ""));
+          mSquare = true;
+        }
+      }
+    });
+  }
+
+  private void initButtonMinusClickListener() {
+    ((Button) findViewById(R.id.butt_minus)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (mEditText == null) {
+          mEditText.setText("");
+        } else {
+          mSmplCalc.setNumOne(Double.parseDouble(mEditText.getText() + ""));
+          mMinus = true;
+          mEditText.setText(null);
+        }
+      }
+    });
+  }
+
+  private void initButtonMultiClickListener() {
+    ((Button) findViewById(R.id.butt_multyplye)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (mEditText == null) {
+          mEditText.setText("");
+        } else {
+          mSmplCalc.setNumOne(Double.parseDouble(mEditText.getText() + ""));
+          mMulti = true;
+          mEditText.setText(null);
+        }
+      }
+    });
+  }
+
+  private void initButtonDivideClickListener() {
+    ((Button) findViewById(R.id.butt_divide)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (mEditText == null) {
+          mEditText.setText("");
+        } else {
+          mSmplCalc.setNumOne(Double.parseDouble(mEditText.getText() + ""));
+          mDivide = true;
+          mEditText.setText(null);
+        }
+      }
+    });
+  }
+
+  private void initButtonEqualClickListener() {
+    ((Button) findViewById(R.id.butt_equal)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mSmplCalc.setNumTwo(Double.parseDouble(mEditText.getText() + ""));
+        if (mPlus == true) {
+          mEditText.setText(mSmplCalc.getNumOne() + mSmplCalc.getNumTwo() + "");
+          mPlus = false;
+        }
+        if (mDivide == true) {
+          mEditText.setText(mSmplCalc.getNumOne() / mSmplCalc.getNumTwo() + "");
+          mDivide = false;
+        }
+        if (mMulti == true) {
+          mEditText.setText(mSmplCalc.getNumOne() * mSmplCalc.getNumTwo() + "");
+          mMulti = false;
+        }
+        if (mMinus == true) {
+          mEditText.setText(mSmplCalc.getNumOne() - mSmplCalc.getNumTwo() + "");
+          mMinus = false;
+        }
+        if (mSquare == true) {
+          mEditText.setText(Math.sqrt(mSmplCalc.getNumOne()) + "");
+          mSquare = false;
+        }
+      }
+    });
+  }
+
+  private void initButtonPlusClickListener() {
+    ((Button) findViewById(R.id.butt_plus)).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (mEditText == null) {
+          mEditText.setText("");
+        } else {
+          mSmplCalc.setNumOne(Double.parseDouble(mEditText.getText() + ""));
+          mPlus = true;
+          mEditText.setText(null);
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onClick(View view) {
+    mSmplCalc.calculate(view.getId());
+    mEditText.setText(mEditText.getText() + mSmplCalc.toString());
   }
 }
---
+++ app/src/main/java/come/geekbrains/myapplication3/SimpleCalculator.java
@@ -0,0 +1,81 @@
+package come.geekbrains.myapplication3;
+
+public class SimpleCalculator {
+
+  private String textField;
+  private double numOne;
+  private double numTwo;
+
+  public double getNumOne() {
+    return numOne;
+  }
+
+  public void setNumOne(double numOne) {
+    this.numOne = numOne;
+  }
+
+  public double getNumTwo() {
+    return numTwo;
+  }
+
+  public void setNumTwo(double numTwo) {
+    this.numTwo = numTwo;
+  }
+
+  public SimpleCalculator() {
+    textField = "0";
+
+  }
+
+  public String getTextField() {
+    return textField;
+  }
+
+  public void setTextField(String textField) {
+    this.textField = textField;
+  }
+
+  public void calculate(int id){
+
+    switch (id) {
+      case (R.id.button_0):
+        textField = "0";
+        break;
+      case (R.id.button_1):
+        textField = "1";
+        break;
+      case (R.id.button_2):
+        textField = "2";
+        break;
+      case (R.id.button_3):
+        textField = "3";
+        break;
+      case (R.id.button_4):
+        textField = "4";
+        break;
+      case (R.id.button_5):
+        textField = "5";
+        break;
+      case (R.id.button_6):
+        textField = "6";
+        break;
+      case (R.id.button_7):
+        textField = "7";
+        break;
+      case (R.id.button_8):
+        textField = "8";
+        break;
+      case (R.id.button_9):
+        textField = "9";
+        break;
+      case (R.id.butt_point):
+        textField = ".";//???????
+        break;
+    }
+  }
+
+  @Override
+  public String toString() {
+    return textField;
+  }
+}
